8933327cbf5ca51dca0a6bcab17ed4c381dabaab
Thaisy Gonçalves <159073811+Thaynix@users.noreply.github.com>
2025-06-30 11:22:33 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,40 +6,38 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.    
-
-
-# Create your objects here.
+# Inicia o EV3
 ev3 = EV3Brick()
 
-motor = Motor(Port.B)
-sencor = ColorSensor(Port.S1)
+# Motores
+motorL = Motor(Port.B)
+motorR = Motor(Port.C)
+# motorCentral = Motor(Port.A)
 
+# Sensores
+sensorCorR = ColorSensor(Port.S1)
+sensorCorL = ColorSensor(Port.S2)
+
+
+# Função andar
+def andar():
+    while True:
+        potenciaL = 50
+        potenciaR = 50
+        motorR.run(potenciaR)
+        motorL.run(potenciaL)
+        wait(1000)  
+
+# Função para sensor de cor
 def sensorCor():
     while True:
-        sensorcor = sencor.color()
-    
-        print("cor: ", sensorcor)
+        sensorR = sensorCorR.color()
+        sensorL = sensorCorL.color()
+        if sensorL and sensorR == Color.BROWN:
+            andar()
+        else:
+            andar.stop()
         wait(1000)
+sensorCor()
 
-def rotmotor():
-    while True:
-        print(sencor.color())
-        while sencor.color() == Color.BLACK:
-            motor.run(500)
-            wait(1000)
-        motor.run(0)
-        ev3.speaker.beep(frequency=1000, duration=500)
-
-rotmotor()
-# sensorCor()
-# motor.run(1000)      
-# wait(5000)
-ev3.speaker.beep()
-
-
-# test_motor.run_target(1000, 90)
-
-ev3.speaker.beep(frequency=1000, duration=500)
+wait(2000)
